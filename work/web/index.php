9d8ceb37c51bd5d9289233829952f8acac62c547
satoyuki <yuki01377@gmail.com>
2020-09-26 10:26:11 +0900

    textareaから値を受け取る

--- work/web/index.php
+++ work/web/index.php
@@ -4,7 +4,8 @@ require('../app/functions.php');
 include('../app/_parts/_header.php');
 ?>
 <form action="result.php" method="get">
-  <input type="text" name="message">
+  <!-- <input type="text" name="message"> -->
+  <textarea name="message" cols="30" rows="10"></textarea>
   <button>send</button>
 </form>
 <?php
--- work/web/result.php
+++ work/web/result.php
@@ -5,7 +5,7 @@ $message = trim(filter_input(INPUT_GET, 'message'));
 $message = $message !== '' ? $message : '...';
 include('../app/_parts/_header.php');
 ?>
-<p><?= h($message); ?></p>
+<p><?= nl2br(h($message)); ?></p>
 <p><a href="index.php">Go back</a></p>
 <?php
 
