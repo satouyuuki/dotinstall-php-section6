de8d70439e5930c7efe00c218c52709e1286384e
satoyuki <yuki01377@gmail.com>
2020-09-26 10:34:08 +0900

    select 複数送信

--- work/web/index.php
+++ work/web/index.php
@@ -4,7 +4,7 @@ require('../app/functions.php');
 include('../app/_parts/_header.php');
 ?>
 <form action="result.php" method="get">
-  <select name="color">
+  <select name="colors[]" multiple>
     <option value="orange">Orange</option>
     <option value="pink">Pink</option>
     <option value="gold">Gold</option>
