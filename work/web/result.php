74b5902ce94f3fecfa3f164b7355c7cd2faa87cc
satoyuki <yuki01377@gmail.com>
2020-09-26 14:33:24 +0900

    postを受け取る処理

--- work/web/result.php
+++ work/web/result.php
@@ -1,8 +1,18 @@
 <?php
 require('../app/functions.php');
 
+$message = trim(filter_input(INPUT_POST, 'message'));
+$message = $message !== '' ? $message : '...';
+
+$filename = '../app/messages.txt';
+$fp = fopen($filename, 'a');
+fwrite($fp, $message . "\n");
+fclose($fp);
+
 include('../app/_parts/_header.php');
+
 ?>
+<p>Message added!</p>
 <p><a href="index.php">Go back</a></p>
 <?php
 
