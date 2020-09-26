2aaea053db2e1dbec012519f8f230938abf06158
satoyuki <yuki01377@gmail.com>
2020-09-26 14:28:48 +0900

    簡易掲示板

--- work/web/index.php
+++ work/web/index.php
@@ -1,10 +1,19 @@
 <?php
 require('../app/functions.php');
 
+$filename = '../app/messages.txt';
+$messages = file($filename, FILE_IGNORE_NEW_LINES);
+
 include('../app/_parts/_header.php');
 ?>
+<ul>
+  <?php foreach($messages as $message): ?>
+    <li><?= h($message); ?></li>
+  <?php endforeach;?>
+</ul>
 <form action="result.php" method="post">
-  <button>send</button>
+  <input type="text" name="message">
+  <button>Post</button>
 </form>
 <?php
 
