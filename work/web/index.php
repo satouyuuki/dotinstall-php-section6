afb0bddfa76faa11d4f382ae9b73c6176fbe65e6
satoyuki <yuki01377@gmail.com>
2020-09-26 10:43:05 +0900

    radioボタンの制御

--- work/web/index.php
+++ work/web/index.php
@@ -4,9 +4,9 @@ require('../app/functions.php');
 include('../app/_parts/_header.php');
 ?>
 <form action="result.php" method="get">
-  <label for=""><input type="checkbox" name="colors[]" value="orange">Orange</label>
-  <label for=""><input type="checkbox" name="colors[]" value="pink">Pink</label>
-  <label for=""><input type="checkbox" name="colors[]" value="gold">Gold</label>
+  <label for=""><input type="radio" name="color" value="orange">Orange</label>
+  <label for=""><input type="radio" name="color" value="pink">Pink</label>
+  <label for=""><input type="radio" name="color" value="gold">Gold</label>
   <button>send</button>
 </form>
 <?php
