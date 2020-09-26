7500aea2a4c18839eef77bc54748a98c418f16e0
satoyuki <yuki01377@gmail.com>
2020-09-26 10:03:20 +0900

    値が空白だったら?

--- work/web/index.php
+++ work/web/index.php
@@ -5,7 +5,6 @@ include('../app/_parts/_header.php');
 ?>
 <form action="result.php" method="get">
   <input type="text" name="message">
-  <input type="text" name="username">
   <button>send</button>
 </form>
 <?php
--- work/web/result.php
+++ work/web/result.php
@@ -1,12 +1,11 @@
 <?php
 require('../app/functions.php');
 
-$message = filter_input(INPUT_GET, 'message');
-$username = filter_input(INPUT_GET, 'username');
-
+$message = trim(filter_input(INPUT_GET, 'message'));
+$message = $message !== '' ? $message : '...';
 include('../app/_parts/_header.php');
 ?>
-<p><?= h($message); ?> by <?= h($username); ?></p>
+<p><?= h($message); ?></p>
 <p><a href="index.php">Go back</a></p>
 <?php
 
