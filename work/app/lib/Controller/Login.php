2ec04663e3e58ae93540944bdf55a7fee1f374d9
satoyuki <yuki01377@gmail.com>
2020-09-27 22:03:58 +0900

    login処理周りのcontrollerとviewをかく

---
+++ work/app/lib/Controller/Login.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace MyApp\Controller;
+
+class Login extends \MyApp\Controller {
+  public function run() {
+    if($this->isLoggedIn()) {
+      // login
+      header('Location: ' . SITE_URL);
+      exit;
+    }
+
+    // get users info
+    if($_SERVER['REQUEST_METHOD'] === 'POST') {
+      $this->postProcess();
+    }
+  }
+
+  protected function postProcess() {
+    // validate
+    try {
+      $this->_validate();
+    } catch (\MyApp\Exception\EmptyPost $e) {
+      $this->setErrors('login', $e->getMessage());
+    }
+
+    // echo "success";
+    // exit;
+
+    $this->setValues('email', $_POST['email']);
+
+    if($this->hasError()) {
+      return;
+    } else {
+      // create user
+      try {
+        $userModel = new \MyApp\Model\User();
+        // うまくいった時はuser情報を返す
+        $user = $userModel->login([
+          'email' => $_POST['email'],
+          'password' => $_POST['password']
+        ]);
+      } catch (\MyApp\Exception\UnmatchEmailOrPassword $e) {
+        $this->setErrors('login', $e->getMessage());
+        return;
+      }
+
+      // login 処理
+      $_SESSION['me'] = $user;
+
+      // redirect to home
+      header('Location: ' . SITE_URL);
+    }
+
+  }
+
+  private function _validate() {
+    if(!isset($_POST['token']) || $_POST['token'] !== $_SESSION['token']) {
+      echo 'Invalid Token!';
+      exit;
+    }
+    if(!isset($_POST['email']) || !isset($_POST['password'])) {
+      echo 'Invalid Form';
+    }
+    if($_POST['email'] === '' || $_POST['password'] === '') {
+      throw new \MyApp\Exception\EmptyPost();
+    }
+  }
+}
--- work/web/login.php
+++ work/web/login.php
@@ -4,9 +4,9 @@
 
 require_once(__DIR__ . '/../app/config/config.php');
 
-// $app = new MyApp\Controller\Login();
+$app = new MyApp\Controller\Login();
 
-// $app->run();
+$app->run();
 
 // echo "login screen";
 // exit;
@@ -22,14 +22,16 @@ require_once(__DIR__ . '/../app/config/config.php');
 </head>
 <body>
   <div id="container">
-    <form action="" method="post">
+    <form action="" method="post" id="login">
+      <input type="hidden" name="token" value="<?= h($_SESSION['token']); ?>">
       <p>
-        <input type="text" name="email" placeholder="email">
+        <input type="text" name="email" placeholder="email" value="<?= isset($app->getValues()->email) ? h($app->getValues()->email) : ''; ?>">
       </p>
       <p>
         <input type="password" name="password" placeholder="password">
       </p>
-      <div class="btn">Log In</div>
+      <p class="err"><?= h($app->getErrors('login')); ?></p>
+      <div class="btn" onclick="document.getElementById('login').submit();">Log In</div>
       <p class="fs12"><a href="/signup.php">Sign Up</a></p>
     </form>
   </div>
