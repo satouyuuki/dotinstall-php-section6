249ba98fa8e151dbcba61c2e73a0ceac5873e9d3
satoyuki <yuki01377@gmail.com>
2020-09-27 19:18:05 +0900

    ログインページのスタイリング

--- work/web/login.php
+++ work/web/login.php
@@ -8,5 +8,29 @@ require_once(__DIR__ . '/../app/config/config.php');
 
 // $app->run();
 
-echo "login screen";
-exit;
+// echo "login screen";
+// exit;
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Log In</title>
+  <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+  <div id="container">
+    <form action="" method="post">
+      <p>
+        <input type="text" name="email" placeholder="email">
+      </p>
+      <p>
+        <input type="password" name="password" placeholder="password">
+      </p>
+      <div class="btn">Log In</div>
+    </form>
+  </div>
+</body>
+</html>
